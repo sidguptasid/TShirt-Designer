f787984e1036ed8c1100d2c627382395c6d3fd6b
Siddharth Gupta <siddharthg21@gmail.com>
2023-08-16 13:37:09 -0400

    Basic Project Initial SetUp

---
+++ client/src/components/ColorPiker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorPiker = () => {
+  return (
+    <div>ColorPiker</div>
+  )
+}
+
+export default ColorPiker
---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPiker from "./ColorPiker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { 
+    CustomButton,
+    AIPicker,
+    ColorPiker,
+    FilePicker,
+    Tab,
+};
